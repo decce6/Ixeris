93fdef02908cf81f6894f8026ff28810251990f3
decce6 <decce6@proton.me>
2025-07-02 12:38:52 +0800

    Fix error description in GLFWErrorCallback

--- common/src/main/java/me/decce/ixeris/glfw/RedirectedGLFWErrorCallbackI.java
+++ common/src/main/java/me/decce/ixeris/glfw/RedirectedGLFWErrorCallbackI.java
@@ -7,10 +7,18 @@ package me.decce.ixeris.glfw;
 
 import me.decce.ixeris.Ixeris;
 import org.lwjgl.glfw.GLFWErrorCallbackI;
+import org.lwjgl.system.MemoryUtil;
 
 public interface RedirectedGLFWErrorCallbackI extends GLFWErrorCallbackI {
     static RedirectedGLFWErrorCallbackI wrap(GLFWErrorCallbackI i) {
-        return (error, description) ->
-                Ixeris.runLaterOnRenderThread(() -> i.invoke(error, description));
+        return (error, description) -> {
+            // Copy the description, as it would be otherwise unavailable when the callback is run
+            String str = MemoryUtil.memUTF8(description);
+            long address = MemoryUtil.memAddress(MemoryUtil.memUTF8(str));
+            Ixeris.runLaterOnRenderThread(() -> {
+                i.invoke(error, address);
+                MemoryUtil.nmemFree(address);
+            });
+        };
     }
 }
