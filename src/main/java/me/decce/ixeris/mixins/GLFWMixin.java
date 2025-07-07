36e5033a2267d5ee5d508ac8312f7043db667317
decce6 <decce6@proton.me>
2025-07-07 16:05:11 +0800

    Initial steps for fixing macOS crash

--- src/main/java/me/decce/ixeris/mixins/GLFWMixin.java
+++ src/main/java/me/decce/ixeris/mixins/GLFWMixin.java
@@ -3,6 +3,7 @@ package me.decce.ixeris.mixins;
 import me.decce.ixeris.Ixeris;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,6 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = GLFW.class, remap = false)
 public class GLFWMixin {
+    @Unique
+    private static boolean ixeris$suppressLogging;
+
     @Inject(method = "glfwInit", at = @At("TAIL"))
     private static void ixeris$glfwInit(CallbackInfoReturnable<Boolean> cir) {
         Ixeris.glfwInitialized = true;
@@ -18,5 +22,17 @@ public class GLFWMixin {
     @Inject(method = "glfwTerminate", at = @At("TAIL"))
     private static void ixeris$glfwTerminate(CallbackInfo ci) {
         Ixeris.glfwInitialized = false;
+    }
+
+    @Inject(method = { "glfwPollEvents", "glfwWaitEvents", "glfwWaitEventsTimeout" }, at = @At("HEAD"), cancellable = true, order = 2000)
+    private static void ixeris$cancelDangerousEventPolling(CallbackInfo ci) {
+        if (!Ixeris.isOnMainThread()) {
+            ci.cancel();
+            if (!ixeris$suppressLogging) {
+                Ixeris.LOGGER.warn("One of the GLFW event polling functions has been called on non-main thread.");
+                Thread.dumpStack();
+                ixeris$suppressLogging = true;
+            }
+        }
     }
 }
