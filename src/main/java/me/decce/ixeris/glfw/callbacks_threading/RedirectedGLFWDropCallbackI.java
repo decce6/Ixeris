c9611f92e4fc54d4df3bc3de5a8faf3f86c4f191
decce6 <decce6@proton.me>
2025-07-13 13:20:24 +0800

    Correctly fix GLFW drop callback

--- src/main/java/me/decce/ixeris/glfw/callbacks_threading/RedirectedGLFWDropCallbackI.java
+++ src/main/java/me/decce/ixeris/glfw/callbacks_threading/RedirectedGLFWDropCallbackI.java
@@ -7,15 +7,16 @@ package me.decce.ixeris.glfw.callbacks_threading;
 
 import me.decce.ixeris.threading.RenderThreadDispatcher;
 import me.decce.ixeris.util.MemoryHelper;
+import org.lwjgl.glfw.GLFWDropCallback;
 import org.lwjgl.glfw.GLFWDropCallbackI;
 
 public interface RedirectedGLFWDropCallbackI extends GLFWDropCallbackI {
     static RedirectedGLFWDropCallbackI wrap(GLFWDropCallbackI i) {
         return (window, count, names) -> {
-            long namesCopy = MemoryHelper.deepCopy(names);
+            long namesCopy = MemoryHelper.deepCopy(names, count, GLFWDropCallback::getName);
             RenderThreadDispatcher.runLater(() -> {
                 i.invoke(window, count, namesCopy);
-                MemoryHelper.free(namesCopy);
+                MemoryHelper.free(namesCopy, count);
             });
         };
     }
--- src/main/java/me/decce/ixeris/util/MemoryHelper.java
+++ src/main/java/me/decce/ixeris/util/MemoryHelper.java
@@ -1,6 +1,10 @@
 package me.decce.ixeris.util;
 
+import org.lwjgl.system.APIUtil;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
+
+import java.util.function.BiFunction;
 
 public class MemoryHelper {
     public static long deepCopy(long address) {
@@ -8,7 +12,19 @@ public class MemoryHelper {
         return MemoryUtil.memAddress(MemoryUtil.memUTF8(str));
     }
 
+    public static long deepCopy(long address, int arrayLength, BiFunction<Long, Integer, String> getter) {
+        String[] strings = new String[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            strings[i] = getter.apply(address, i);
+        }
+        return APIUtil.apiArrayi(MemoryStack.stackGet(), MemoryUtil::memUTF8, strings);
+    }
+
     public static void free(long address) {
         MemoryUtil.nmemFree(address);
+    }
+
+    public static void free(long address, int arrayLength) {
+        APIUtil.apiArrayFree(address, arrayLength);
     }
 }
