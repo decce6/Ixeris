4ca2781d64ebf371e1ddb2bbd372f46b1104da53
decce6 <decce6@proton.me>
2025-07-24 14:17:40 +0800

    Use array set in standard cursor cache

--- src/main/java/me/decce/ixeris/glfw/state_caching/global/GlfwCachedStandardCursor.java
+++ src/main/java/me/decce/ixeris/glfw/state_caching/global/GlfwCachedStandardCursor.java
@@ -1,18 +1,18 @@
 package me.decce.ixeris.glfw.state_caching.global;
 
-import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongArraySet;
 import org.lwjgl.glfw.GLFW;
 
 public class GlfwCachedStandardCursor {
     private boolean disposed;
-    private final LongOpenHashSet using;
+    private final LongArraySet using;
     private final int shape;
     private final long cursor;
 
     public GlfwCachedStandardCursor(int shape, long cursor) {
         this.shape = shape;
         this.cursor = cursor;
-        this.using = new LongOpenHashSet(1);
+        this.using = new LongArraySet(1);
     }
 
     public long cursor() {
