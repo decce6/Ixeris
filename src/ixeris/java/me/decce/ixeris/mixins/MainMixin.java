8ae5f5ed5cbeb8836a5f7ec6926b32185840f149
decce6 <decce6@proton.me>
2025-10-31 13:51:47 +0800

    Startup the main thread a little later
    Related: #36

--- src/ixeris/java/me/decce/ixeris/mixins/MainMixin.java
+++ src/ixeris/java/me/decce/ixeris/mixins/MainMixin.java
@@ -47,7 +47,9 @@ public class MainMixin {
         Thread.currentThread().setPriority(Ixeris.getConfig().getEventPollingThreadPriority());
 
         //noinspection ConstantValue
-        while (Minecraft.getInstance() == null) {
+        while (Minecraft.getInstance() == null ||
+                Minecraft.getInstance().keyboardHandler == null ||
+                Minecraft.getInstance().mouseHandler == null) {
             Thread.yield();
         }
 
