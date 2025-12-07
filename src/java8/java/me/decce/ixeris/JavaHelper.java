7f284121165b63ba69b1e5347efb45987a6120da
decce6 <decce6@proton.me>
2025-12-07 20:04:24 +0800

    Fix java version parsing

--- src/java8/java/me/decce/ixeris/JavaHelper.java
+++ src/java8/java/me/decce/ixeris/JavaHelper.java
@@ -1,10 +1,12 @@
 package me.decce.ixeris;
 
+import java.util.regex.Pattern;
+
 public class JavaHelper {
     public static final boolean JAVA_SUPPORTED;
 
     static {
         String javaVersion = System.getProperty("java.version");
-        JAVA_SUPPORTED = !javaVersion.startsWith("1.") && Integer.parseInt(javaVersion.substring(0, 2)) >= 17;
+        JAVA_SUPPORTED = !javaVersion.startsWith("1.") && Integer.parseInt(javaVersion.split(Pattern.quote("."))[0]) >= 17;
     }
 }
