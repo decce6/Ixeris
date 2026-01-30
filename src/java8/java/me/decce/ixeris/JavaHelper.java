90d44e4a3aad4a9f0ed641558cdccf140bab96f6
decce6 <decce6@proton.me>
2026-01-30 21:05:09 +0800

    Fix Java version parsing

--- src/java8/java/me/decce/ixeris/JavaHelper.java
+++ src/java8/java/me/decce/ixeris/JavaHelper.java
@@ -4,10 +4,25 @@ import java.util.regex.Pattern;
 
 public class JavaHelper {
     public static final boolean JAVA_SUPPORTED;
+    public static final int MINIMUM_MAJOR = 17;
+    public static String message;
 
     static {
         String javaVersion = System.getProperty("java.version");
-        String major = javaVersion.split(Pattern.quote("."))[0].replace("-ea", "");
-        JAVA_SUPPORTED = !javaVersion.startsWith("1.") && Integer.parseInt(major) >= 17;
+        if (javaVersion.startsWith("1.")) {
+            JAVA_SUPPORTED = false;
+        }
+        else {
+            int major;
+            try {
+                String majorString = javaVersion.split(Pattern.quote("."))[0];
+                int i = majorString.indexOf('-'); // some builds may contain version strings like 26-ea, 26-beta
+                major = Integer.parseInt(i == -1 ? majorString : majorString.substring(0, i));
+            } catch (Exception e) {
+                message = "Failed to parse java version, assuming supported java: " + javaVersion;
+                major = MINIMUM_MAJOR;
+            }
+            JAVA_SUPPORTED = major >= MINIMUM_MAJOR;
+        }
     }
 }
