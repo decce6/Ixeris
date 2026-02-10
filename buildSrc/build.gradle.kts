38858efec8b09f342186a6afc13735eb371da6b7
decce6 <decce6@proton.me>
2026-02-10 08:37:39 +0800

    Update mod publish plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
-    implementation("me.modmuss50:mod-publish-plugin:0.8.+")
+    implementation("me.modmuss50:mod-publish-plugin:1.1.0")
     implementation("dev.kikugie:stonecutter:0.7.10")
 }
