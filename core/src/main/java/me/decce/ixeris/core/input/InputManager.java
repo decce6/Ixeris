a4483c7d65dfba54ce5f7bc8d8f055ac564a003b
decce6 <decce6@proton.me>
2026-02-21 07:56:07 +0800

    Take over event loop from GLFW
    Previously, if a WM_INPUT message was generated between the call to processInput and glfwPollEvents, it would be silently consumed by GLFW, causing loss of input message. This commit completely takes over event loop from GLFW, using the appropriate filters to skip WM_INPUT messages.
    
    Additionally, this has allowed us to move the processInput call to *after* pollEvents, further reduing input latency.

--- core/src/main/java/me/decce/ixeris/core/input/InputManager.java
+++ core/src/main/java/me/decce/ixeris/core/input/InputManager.java
@@ -3,10 +3,21 @@ package me.decce.ixeris.core.input;
 import me.decce.ixeris.core.Ixeris;
 import me.decce.ixeris.core.glfw.state_caching.GlfwCacheManager;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.windows.MSG;
+
+import static org.lwjgl.system.windows.User32.DispatchMessage;
+import static org.lwjgl.system.windows.User32.PM_REMOVE;
+import static org.lwjgl.system.windows.User32.PeekMessage;
+import static org.lwjgl.system.windows.User32.PostMessage;
+import static org.lwjgl.system.windows.User32.TranslateMessage;
+import static org.lwjgl.system.windows.User32.WM_INPUT;
+import static org.lwjgl.system.windows.User32.WM_QUIT;
 
 public class InputManager {
     // The current GLFW window which has cursor input mode set to GLFW_CURSOR_DISABLED
     private long currentInputWindow;
+    private boolean receivedWMQuit;
+    private int wmQuitExitCode;
 
     public long window() {
         return currentInputWindow;
@@ -22,10 +33,43 @@ public class InputManager {
 
     public void pollEvents() {
         if (buffered() && cursorGrabbed() && GlfwCacheManager.hasWindowCache(currentInputWindow)) {
+            _pollEvents();
             GlfwCacheManager.getWindowCache(currentInputWindow).rawInput().processInput();
         }
-        GLFW.glfwPollEvents();
+        else {
+            GLFW.glfwPollEvents();
+        }
+    }
 
+    private void _pollEvents() {
+        MSG msg = MSG.malloc();
+
+        // Process messages *before* WM_INPUT
+        while (PeekMessage(msg, 0, 0, WM_INPUT - 1, PM_REMOVE)) {
+            processMessage(msg);
+        }
+
+        // Process messages *after* WM_INPUT
+        while(PeekMessage(msg, 0, WM_INPUT + 1, -1, PM_REMOVE)) {
+            processMessage(msg);
+        }
+
+        if (receivedWMQuit) {
+            receivedWMQuit = false;
+            PostMessage(null, 0, WM_QUIT, wmQuitExitCode, 0);
+            GLFW.glfwPollEvents();
+        }
+
+        msg.free();
+    }
+
+    private void processMessage(MSG msg) {
+        if (msg.message() == WM_QUIT) {
+            receivedWMQuit = true; // GLFW processes this message in the event loop, not window procedure, so we repost the event later and call glfwPollEvents
+            wmQuitExitCode = (int) msg.wParam();
+        }
+        TranslateMessage(msg);
+        DispatchMessage(msg);
     }
 
     public void grab(long window) {
