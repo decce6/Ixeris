63916b336c73576681cfe34646c941d13ea8efb4
decce6 <decce6@proton.me>
2026-03-03 08:44:39 +0800

    Adjust injection points to improve input latency

--- src/ixeris/java/me/decce/ixeris/mixins/MinecraftMixin.java
+++ src/ixeris/java/me/decce/ixeris/mixins/MinecraftMixin.java
@@ -1,13 +1,10 @@
 package me.decce.ixeris.mixins;
 
-import me.decce.ixeris.IxerisMod;
 import me.decce.ixeris.VersionCompatUtils;
 import me.decce.ixeris.core.Ixeris;
 import me.decce.ixeris.core.threading.MainThreadDispatcher;
 import me.decce.ixeris.core.threading.RenderThreadDispatcher;
-import me.decce.ixeris.core.util.PlatformHelper;
 import net.minecraft.client.Minecraft;
-import org.lwjgl.opengl.CGL;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,20 +12,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Minecraft.class, priority = 500)
 public abstract class MinecraftMixin {
-    @Inject(method = "runTick", at = @At("HEAD"))
+    @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Ljava/lang/Thread;yield()V"))
     private void ixeris$pollEvents(boolean tick, CallbackInfo ci) {
         MainThreadDispatcher.requestPollEvents();
     }
 
-    //? if forge && <= 1.20.4 {
-    /*@Inject(method = "runTick", at = @At(value = "INVOKE", target = "Ljava/lang/Thread;yield()V", shift = At.Shift.AFTER))
-            *///?} else {
-    @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Ljava/lang/Thread;yield()V", shift = At.Shift.AFTER), order = 10000)
-     //?}
+    @Inject(method = "runTick", at = {
+            // Process callbacks right before Minecraft processes mouse input, to maximally reduce input latency
+            @At(value = "CONSTANT", args = "stringValue=mouse"),
+            @At("HEAD")
+    }, require = 1)
     private void ixeris$replayQueue(boolean tick, CallbackInfo ci) {
-        VersionCompatUtils.profilerPopPush("callback"); // Pop the "yield" section and push ours
+        VersionCompatUtils.profilerPush("callback");
         RenderThreadDispatcher.replayQueue();
-        // We injected before the "pop" call for the "yield" section, do not pop here
+        VersionCompatUtils.profilerPop();
     }
 
     @Inject(method = "destroy", at = @At(value = "INVOKE", target = "Ljava/lang/System;exit(I)V"))
