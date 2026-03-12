b3d13e4ed644f7516be232ee816031c70c2c13c7
decce6 <decce6@proton.me>
2026-03-13 07:26:00 +0800

    Revert "No longer cancel GLFW raw input mode setting as it is no longer needed (event loop taken over)"
    This reverts commit 05a9f4d598227fee69830c70609fc8e0764cf5a2.

--- core/src/main/java/me/decce/ixeris/core/mixins/GLFWMixin.java
+++ core/src/main/java/me/decce/ixeris/core/mixins/GLFWMixin.java
@@ -62,6 +62,9 @@ public class GLFWMixin {
                     Ixeris.input().release(window);
                 }
             }
+            if (mode == GLFW.GLFW_RAW_MOUSE_MOTION) {
+                ci.cancel();
+            }
         }
     }
 }
