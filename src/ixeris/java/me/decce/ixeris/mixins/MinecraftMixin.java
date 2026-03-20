7ddf06208a3ec93db7eb5475fb26b6bb7f79a589
decce6 <decce6@proton.me>
2026-03-21 05:37:21 +0800

    Fix missing import for 26.1

--- src/ixeris/java/me/decce/ixeris/mixins/MinecraftMixin.java
+++ src/ixeris/java/me/decce/ixeris/mixins/MinecraftMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Minecraft.class, priority = 500)
