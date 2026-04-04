a722ffdb0e4a0f5025613edb0ddc0b643b9dc46f
decce6 <decce6@proton.me>
2026-04-04 19:21:27 +0800

    Fix JiJ discovery on 1.21.1
    This commit fixes the `java.nio.file.ProviderNotFoundException: Provider "jar" not found` errors.

--- service-neoforge/src/main/java/me/decce/ixeris/neoforge/core/IxerisModLocator.java
+++ service-neoforge/src/main/java/me/decce/ixeris/neoforge/core/IxerisModLocator.java
@@ -1,28 +1,34 @@
 package me.decce.ixeris.neoforge.core;
 
-import cpw.mods.jarhandling.JarContents;
+import cpw.mods.jarhandling.SecureJar;
 import net.neoforged.fml.loading.FMLLoader;
-import net.neoforged.neoforgespi.ILaunchContext;
+import net.neoforged.fml.loading.moddiscovery.locators.JarInJarDependencyLocator;
+import net.neoforged.fml.loading.moddiscovery.readers.JarModsDotTomlModFileReader;
+import net.neoforged.neoforgespi.locating.IDependencyLocator;
 import net.neoforged.neoforgespi.locating.IDiscoveryPipeline;
-import net.neoforged.neoforgespi.locating.IModFileCandidateLocator;
-import net.neoforged.neoforgespi.locating.IncompatibleFileReporting;
-import net.neoforged.neoforgespi.locating.ModFileDiscoveryAttributes;
+import net.neoforged.neoforgespi.locating.IModFile;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 
-public class IxerisModLocator implements IModFileCandidateLocator {
+public class IxerisModLocator extends JarInJarDependencyLocator implements IDependencyLocator {
     @Override
-    public void findCandidates(ILaunchContext context, IDiscoveryPipeline pipeline) {
+    public void scanMods(List<IModFile> loadedMods, IDiscoveryPipeline pipeline) {
         if (!FMLLoader.getDist().isClient()) {
             return;
         }
         try {
-            final var resource = this.getClass().getResource("/META-INF/jarjar/");
+            var secureJar = SecureJar.from(Path.of(IxerisModLocator.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+            var resource = IxerisModLocator.class.getResource("/META-INF/jarjar/");
             try (var stream = Files.walk(Path.of(Objects.requireNonNull(resource).toURI()), 1)) {
                 stream.filter(path -> path.getFileName().toString().endsWith("-mod.jar"))
-                        .forEach(path -> pipeline.addJarContent(JarContents.of(path), ModFileDiscoveryAttributes.DEFAULT, IncompatibleFileReporting.ERROR));
+                        .forEach(path -> {
+                            var modFile = IModFile.create(secureJar, JarModsDotTomlModFileReader::manifestParser);
+                            var mod = loadModFileFrom(modFile, Path.of("META-INF", "jarjar", path.getFileName().toString()), pipeline).orElseThrow();
+                            pipeline.addModFile(mod);
+                        });
             }
         } catch (Throwable t) {
             throw new RuntimeException("Loading Ixeris JiJ mod", t);
