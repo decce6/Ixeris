38d257bc1f61f6cf0f7b5901a129c691c70d873d
decce6 <decce6@proton.me>
2026-05-15 20:50:45 +0800

    Improve API documentation, add `runAsyncOnMainThread`

--- src/api/java/me/decce/ixeris/api/IxerisApi.java
+++ src/api/java/me/decce/ixeris/api/IxerisApi.java
@@ -74,7 +74,7 @@ public class IxerisApi {
      * Execute the provided {@link Runnable} object on the main thread.
      * </p>
      * <p>
-     * When fullyBlockingMode is enabled in Ixeris config, this is equivalent to calling
+     * When fullyBlockingMode (false by default) is enabled in Ixeris config, this is equivalent to calling
      * {@link IxerisApi#runNowOnMainThread(Runnable)}. Otherwise, this is equivalent to calling
      * {@link IxerisApi#runLaterOnMainThread(Runnable)}.
      * </p>
@@ -89,8 +89,14 @@ public class IxerisApi {
     }
 
     /**
+     * <p>
      * Execute the provided {@link Runnable} object instantly on the main thread. If the current thread is not the main thread, it may
      * be blocked until the main thread finishes execution.
+     * </p>
+     * <p>
+     * Make sure to use this method very sparingly. If you only need to know whether the execution has finished, use
+     * {@link IxerisApi#runAsyncOnMainThread(Runnable)} for optimal performance.
+     * </p>
      */
     public void runNowOnMainThread(Runnable runnable) {
         if (isEnabled()) {
@@ -114,8 +120,42 @@ public class IxerisApi {
     }
 
     /**
+     * <p>
+     * Execute the provided {@link Runnable} object asynchronously on the main thread.
+     * </p>
+     * <p>
+     * When there is no need to know whether the execution has finished, use {@link IxerisApi#runOnMainThread(Runnable)}.
+     * </p>
+     * @return An {@link IxerisFuture} object which is an instance of {@link Future}. Use {@link IxerisFuture#isDone()}
+     * to check if the execution has finished, and {@link IxerisFuture#get()} to wait for execution to finish.
+     */
+    public IxerisFuture<Void> runAsyncOnMainThread(Runnable runnable) {
+        IxerisFuture<Void> future = new IxerisFuture<>();
+        if (isEnabled()) {
+            MainThreadDispatcher.runLater(() -> {
+                runnable.run();
+                future.set(null);
+            });
+        }
+        else {
+            runnable.run();
+            future.set(null);
+        }
+        return future;
+    }
+
+    /**
+     * <p>
      * Retrieve value from the provided {@link Supplier} object instantly on the main thread. If the current thread is
      * not the main thread, it may be blocked until the value is retrieved from the main thread.
+     * </p>
+     * <p>
+     * When a return value is not required, use {@link IxerisApi#runOnMainThread(Runnable)}.
+     * </p>
+     * <p>
+     * Make sure to use this method very sparingly. If you need to frequently query values from the main thread, use
+     * {@link IxerisApi#queryAsync(Supplier)} for optimal performance.
+     * </p>
      */
     public <T> T query(Supplier<T> supplier) {
         if (isEnabled()) {
@@ -129,7 +169,9 @@ public class IxerisApi {
     /**
      * Retrieve value from the provided {@link Supplier} object asynchronously from the main thread.
      *
-     * @return An {@link IxerisFuture} object which is an instance of {@link Future}
+     * @return An {@link IxerisFuture} object which is an instance of {@link Future}. Use {@link IxerisFuture#isDone()}
+     * to check if the execution has finished and {@link IxerisFuture#get()} to retrieve the return value (blocks if not
+     * finished)
      */
     public <T> IxerisFuture<T> queryAsync(Supplier<T> supplier) {
         IxerisFuture<T> future = new IxerisFuture<>();
