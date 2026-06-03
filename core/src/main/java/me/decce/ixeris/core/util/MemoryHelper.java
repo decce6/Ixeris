807fb1456dccb5166623b21b7f336257ac24c995
decce6 <decce6@proton.me>
2026-06-03 14:08:40 +0800

    Fix memory operation null safety

--- core/src/main/java/me/decce/ixeris/core/util/MemoryHelper.java
+++ core/src/main/java/me/decce/ixeris/core/util/MemoryHelper.java
@@ -8,6 +8,9 @@ import java.util.function.BiFunction;
 
 public class MemoryHelper {
     public static long copyIntArray(long address, int size) {
+        if (address == 0L) {
+            return 0L;
+        }
         var buffer = MemoryUtil.memIntBuffer(address, size);
         var copied = MemoryUtil.memAllocInt(size);
         copied.put(buffer).flip();
@@ -15,11 +18,17 @@ public class MemoryHelper {
     }
 
     public static long copyString(long address) {
+        if (address == 0L) {
+            return 0L;
+        }
         String str = MemoryUtil.memUTF8(address);
         return MemoryUtil.memAddress(MemoryUtil.memUTF8(str));
     }
 
     public static long copyStringArray(long address, int arrayLength, BiFunction<Long, Integer, String> getter) {
+        if (address == 0L) {
+            return 0L;
+        }
         String[] strings = new String[arrayLength];
         for (int i = 0; i < arrayLength; i++) {
             strings[i] = getter.apply(address, i);
@@ -28,10 +37,14 @@ public class MemoryHelper {
     }
 
     public static void free(long address) {
-        MemoryUtil.nmemFree(address);
+        if (address != 0L) {
+            MemoryUtil.nmemFree(address);
+        }
     }
 
     public static void free(long address, int arrayLength) {
-        APIUtil.apiArrayFree(address, arrayLength);
+        if (address != 0L) {
+            APIUtil.apiArrayFree(address, arrayLength);
+        }
     }
 }
