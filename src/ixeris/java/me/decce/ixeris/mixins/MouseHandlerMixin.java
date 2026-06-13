5bd925e59d1602e70c08e821c2f1076a39d61493
decce6 <decce6@proton.me>
2026-06-13 20:23:56 +0800

    Suppress cursor pos callbacks when grabbing/releasing cursor

--- src/ixeris/java/me/decce/ixeris/mixins/MouseHandlerMixin.java
+++ src/ixeris/java/me/decce/ixeris/mixins/MouseHandlerMixin.java
@@ -2,6 +2,9 @@ package me.decce.ixeris.mixins;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import me.decce.ixeris.VersionCompatUtils;
+import me.decce.ixeris.core.glfw.callback_dispatcher.CursorPosCallbackDispatcher;
+import me.decce.ixeris.core.threading.MainThreadDispatcher;
 import me.decce.ixeris.core.threading.RenderThreadDispatcher;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
@@ -20,22 +23,32 @@ public abstract class MouseHandlerMixin {
     @WrapMethod(method = "grabMouse")
     private void ixeris$wrapGrabMouse(Operation<Void> original) {
         var shouldGrab = this.minecraft.isWindowActive() && !this.mouseGrabbed;
+        var dispatcher = CursorPosCallbackDispatcher.get(VersionCompatUtils.getMinecraftWindow());
+        if (shouldGrab) {
+            dispatcher.suppressCallbacks();
+        }
 
         original.call();
 
         if (shouldGrab) {
             RenderThreadDispatcher.clearQueuedCursorPosCallbacks();
+            MainThreadDispatcher.run(dispatcher::unsuppressCallbacks);
         }
     }
 
     @WrapMethod(method = "releaseMouse")
     private void ixeris$wrapReleaseMouse(Operation<Void> original) {
         var shouldRelease = this.mouseGrabbed;
+        var dispatcher = CursorPosCallbackDispatcher.get(VersionCompatUtils.getMinecraftWindow());
+        if (shouldRelease) {
+            dispatcher.suppressCallbacks();
+        }
 
         original.call();
 
         if (shouldRelease) {
             RenderThreadDispatcher.clearQueuedCursorPosCallbacks();
+            MainThreadDispatcher.run(dispatcher::unsuppressCallbacks);
         }
     }
 }
