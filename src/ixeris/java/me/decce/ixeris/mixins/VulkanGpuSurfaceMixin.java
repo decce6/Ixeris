1646241073ab1f587a86522a00f46d61f8cc8667
decce6 <decce6@proton.me>
2026-06-26 08:19:23 +0800

    Fix crash when resizing framebuffer

--- src/ixeris/java/me/decce/ixeris/mixins/VulkanGpuSurfaceMixin.java
+++ src/ixeris/java/me/decce/ixeris/mixins/VulkanGpuSurfaceMixin.java
@@ -9,36 +9,47 @@ import com.mojang.blaze3d.systems.SurfaceException;
 import com.mojang.blaze3d.vulkan.VulkanGpuSurface;
 import me.decce.ixeris.core.Ixeris;
 import me.decce.ixeris.VersionCompatUtils;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VulkanGpuSurface.class)
 public abstract class VulkanGpuSurfaceMixin {
-    @Unique
-    private boolean ixeris$noRetryConfigure;
+    @Shadow
+    private boolean swapchainOutOfDate;
+    @Shadow
+    private boolean swapchainSuboptimal;
+    @Shadow
+    public abstract void acquireNextTexture() throws SurfaceException;
 
     @ModifyExpressionValue(method = "present", at = @At(value = "NEW", target = "(Ljava/lang/String;)Lcom/mojang/blaze3d/systems/SurfaceException;"), allow = 1)
     private SurfaceException ixeris$recreateSwapchain(SurfaceException original) {
         VersionCompatUtils.reconfigureSwapchain();
+        this.swapchainOutOfDate = false;
+        this.swapchainSuboptimal = false;
         return null;
     }
 
     @Inject(method = "configure", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/SurfaceException;<init>(Ljava/lang/String;)V"), allow = 1, cancellable = true)
     private void ixeris$suppressEventPolling(GpuSurface.Configuration config, CallbackInfo ci) {
-        if (!ixeris$noRetryConfigure) {
-            try {
-                Ixeris.suppressEventPolling();
-                ixeris$noRetryConfigure = true;
-                VersionCompatUtils.reconfigureSwapchain();
-                ci.cancel();
-            }
-            finally {
-                ixeris$noRetryConfigure = false;
-                Ixeris.unsuppressEventPolling();
-            }
+        try {
+            Ixeris.suppressEventPolling();
+            VersionCompatUtils.reconfigureSwapchain();
+            ci.cancel();
         }
+        finally {
+            Ixeris.unsuppressEventPolling();
+        }
+    }
+
+    @Inject(method = "acquireNextTexture", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/SurfaceException;<init>(Ljava/lang/String;)V"), allow = 1, cancellable = true)
+    private void ixeris$acquireNextTexture(CallbackInfo ci) throws SurfaceException {
+        this.swapchainOutOfDate = false;
+        this.swapchainSuboptimal = false;
+        VersionCompatUtils.reconfigureSwapchain();
+        ci.cancel();
+        this.acquireNextTexture();
     }
 }
 *///? } else {
