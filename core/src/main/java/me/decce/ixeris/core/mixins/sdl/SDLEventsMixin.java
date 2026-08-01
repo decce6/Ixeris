b1ae218ac5c908bc14764d02bf10e77b1da630a0
decce6 <decce6@proton.me>
2026-08-01 09:26:54 +0800

    Implement SDL_PeepEvents support

--- core/src/main/java/me/decce/ixeris/core/mixins/sdl/SDLEventsMixin.java
+++ core/src/main/java/me/decce/ixeris/core/mixins/sdl/SDLEventsMixin.java
@@ -4,6 +4,7 @@ import me.decce.ixeris.core.Ixeris;
 import me.decce.ixeris.core.sdl.SdlEventHandler;
 import me.decce.ixeris.core.threading.MainThreadDispatcher;
 import org.lwjgl.sdl.SDLEvents;
+import org.lwjgl.sdl.SDL_Event;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,6 +21,22 @@ public class SDLEventsMixin {
             else {
                 throw new IllegalStateException();
             }
+        }
+    }
+
+    @Inject(method = "nSDL_PeepEvents", at = @At("HEAD"), cancellable = true)
+    private static void ixeris$nSDL_PeepEvent(long events, int numevents, int action, int minType, int maxType, CallbackInfoReturnable<Integer> cir) {
+        if (action == SDLEvents.SDL_GETEVENT && Ixeris.getEventHandler() instanceof SdlEventHandler sdlEventHandler) {
+            if (minType != SDLEvents.SDL_EVENT_FIRST || maxType != SDLEvents.SDL_EVENT_LAST) {
+                throw new RuntimeException("Unsupported arguments for SDL_PeepEvents: minType=" + minType + ", maxType=" + maxType);
+            }
+            int i;
+            for (i = 0; i < numevents; i++) {
+                if (!sdlEventHandler.readEvents(events + (long)(SDL_Event.SIZEOF) * i)) {
+                    break;
+                }
+            }
+            cir.setReturnValue(i);
         }
     }
 }
